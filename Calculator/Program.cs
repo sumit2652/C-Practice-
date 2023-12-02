963839830a54456372cb0025a17a6d67c116bfa2
sumsi <sumit.singh@netcompany.com>
2023-12-02 14:12:02 +0000

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = calculate();
+            Console.WriteLine($"This is the result for adding: {result}");
+            Console.Read();
+        }
+
+        public static int calculate()
+        {
+            Console.WriteLine("Please enter the first number");
+            string num1input = Console.ReadLine();
+            Console.WriteLine("Please enter secound number");
+            string num2input = Console.ReadLine();
+
+            int num1 = int.Parse(num1input);
+            int num2 = int.Parse(num2input);
+
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
